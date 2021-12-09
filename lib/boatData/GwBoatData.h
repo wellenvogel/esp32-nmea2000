d47c19126fec079dbdc8e403e6ef0e25c7dc52b1
wellenvogel <andreas@wellenvogel.de>
2021-12-09 17:18:48 +0100

    use separate formats for days and seconds

--- lib/boatData/GwBoatData.h
+++ lib/boatData/GwBoatData.h
@@ -37,6 +37,8 @@ class GwBoatItemBase{
         GWSC(mtr2nm);
         GWSC(formatDop);
         GWSC(formatRot);
+        GWSC(formatDate);
+        GWSC(formatTime);
         typedef std::map<String,GwBoatItemBase*> GwBoatItemMap;
     protected:
         int type;
@@ -188,7 +190,7 @@ class GwBoatData{
     GWBOATDATA(double,Altitude,4000,formatFixed0)
     GWBOATDATA(double,WaterDepth,4000,formatDepth)
     GWBOATDATA(double,DepthTransducer,4000,formatDepth)
-    GWBOATDATA(double,SecondsSinceMidnight,4000,formatFixed0)
+    GWBOATDATA(double,SecondsSinceMidnight,4000,formatTime)
     GWBOATDATA(double,WaterTemperature,4000,kelvinToC)
     GWBOATDATA(double,XTE,4000,formatXte)
     GWBOATDATA(double,DTW,4000,mtr2nm)
@@ -197,7 +199,7 @@ class GwBoatData{
     GWBOATDATA(double,WPLongitude,4000,formatLongitude)
     GWBOATDATA(uint32_t,Log,16000,mtr2nm)
     GWBOATDATA(uint32_t,TripLog,16000,mtr2nm)
-    GWBOATDATA(uint32_t,DaysSince1970,4000,formatFixed0)
+    GWBOATDATA(uint32_t,DaysSince1970,4000,formatDate)
     GWBOATDATA(int16_t,Timezone,8000,formatFixed0)
     GWSPECBOATDATA(GwBoatDataSatList,SatInfo,GwSatInfoList::lifeTime,formatFixed0);
     public:
