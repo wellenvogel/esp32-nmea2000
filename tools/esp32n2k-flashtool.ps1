56b9dc11dad06e4ea6db45c5f73ecffd83cad96f
wellenvogel <andreas@wellenvogel.de>
2021-12-25 18:53:55 +0100

    add flashtool.pyz to git

--- tools/esp32n2k-flashtool.ps1
+++
@@ -1,8 +0,0 @@
-@echo off
-Rem Make powershell read this file, skip a number of lines, and execute it.
-Rem This works around .ps1 bad file association as non executables.
-PowerShell -Command "Get-Content '%~dpnx0' | Select-Object -Skip 6 | Out-String | Invoke-Expression"
-pause
-goto :eof
-# Start of PowerShell script here
-Write-Host "Hello World!"
