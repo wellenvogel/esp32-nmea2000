4ecadf4d776894a94d1044023cd640df4bbfb471
wt <norbert.walter@ims-gmbh.de>
2022-01-13 15:23:01 +0100

    changes

--- lib/obp60task/GwOBP60Task.h
+++ lib/obp60task/GwOBP60Task.h
@@ -3,10 +3,12 @@
 #include "GwApi.h"
 //we only compile for some boards
 #ifdef BOARD_NODEMCU32S_OBP60
-    //CAN NMEA2000
+    // CAN NMEA2000
     #define ESP32_CAN_TX_PIN GPIO_NUM_13
     #define ESP32_CAN_RX_PIN GPIO_NUM_12
-    //RS485 NMEA0183
+    // Bus load in 50mA steps
+    #define N2K_LOAD_LEVEL 5 (250mA max bus load with back light on)
+    // RS485 NMEA0183
     #define GWSERIAL_TX 26
     #define GWSERIAL_RX 14
     #define GWSERIAL_MODE "UNI"
