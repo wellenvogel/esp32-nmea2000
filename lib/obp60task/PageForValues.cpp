12770cdd9e4acb0e01e91a37e8071da4163ffd20
norbert-walter <norbert-walter@web.de>
2022-02-03 15:49:52 +0100

    first new version with new structures

---
+++ lib/obp60task/PageForValues.cpp
@@ -0,0 +1,36 @@
+#include "Pagedata.h"
+
+class PageForValues : public Page
+{
+public:
+    virtual void display(CommonData &commonData, PageData &pageData)
+    {
+        GwLog *logger = commonData.logger;
+        for (int i = 0; i < 4; i++)
+        {
+            GwApi::BoatValue *value = pageData.values[i];
+            if (value == NULL)
+                continue;
+            LOG_DEBUG(GwLog::LOG, "drawing at PageforValues(%d), p=%s,v=%f",
+                      i,
+                      value->getName().c_str(),
+                      value->valid ? value->value : -1.0);
+        }
+    };
+};
+
+static Page *createPage(CommonData &){
+    return new PageForValues();
+}
+/**
+ * with the code below we make this page known to the PageTask
+ * we give it a type (name) that can be selected in the config
+ * we define which function is to be called
+ * and we provide the number of user parameters we expect
+ * this will be number of BoatValue pointers in pageData.values
+ */
+PageDescription registerPageForValues(
+    "forValues",
+    createPage,
+    4
+);
---
+++ lib/obp60task/obp60task.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "GwApi.h"
+//we only compile for some boards
+#ifdef BOARD_NODEMCU32S_OBP60
+// CAN NMEA2000
+#define ESP32_CAN_TX_PIN GPIO_NUM_13
+#define ESP32_CAN_RX_PIN GPIO_NUM_12
+// Bus load in 50mA steps
+#define N2K_LOAD_LEVEL 5 // 5x50mA = 250mA max bus load with back light on
+// RS485 NMEA0183
+#define GWSERIAL_TX 26
+#define GWSERIAL_RX 14
+#define GWSERIAL_MODE "UNI"
+
+// Init OBP60 Task
+void OBP60Init(GwApi *param);
+DECLARE_INITFUNCTION(OBP60Init);
+
+// OBP60 Task
+void OBP60Task(GwApi *param);
+DECLARE_USERTASK_PARAM(OBP60Task, 25000)    // Need 25k RAM as stack size
+DECLARE_CAPABILITY(obp60,true);
+
+#endif
