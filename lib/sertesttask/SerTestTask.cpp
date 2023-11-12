f885642915bb83f29db9090d9abbe4c82b9ee961
wellenvogel <andreas@wellenvogel.de>
2023-11-12 18:03:10 +0100

    allow to change baud rate for serial test

--- lib/sertesttask/SerTestTask.cpp
+++ lib/sertesttask/SerTestTask.cpp
@@ -4,7 +4,7 @@
 void sertest(GwApi *api){
     GwLog *logger=api->getLogger();
     LOG_DEBUG(GwLog::LOG,"ser test task started");
-    Serial2.begin(9600,SERIAL_8N1,-1,GWSERIAL_RX);
+    Serial2.begin(api->getConfig()->getInt(GwConfigDefinitions::serialBaud), SERIAL_8N1,-1,GWSERIAL_RX);
     while (true){
         const char * T="SERTESTXXX";
         LOG_DEBUG(GwLog::LOG,"sending %s",T);
