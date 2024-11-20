1bc896dfd791a337f6020013195cc9ed11a115cf
Tobias E <Tobias.edler@gmail.com>
2024-11-20 09:20:50 +0000

    modified:   lib/obp60task/PageRollPitch.cpp
        --> fixed Typo

--- lib/obp60task/PageRollPitch.cpp
+++ lib/obp60task/PageRollPitch.cpp
@@ -353,7 +353,8 @@ PageDescription registerPageRollPitch(
     "RollPitch",        // Page name
     createPage,         // Action
     0,                  // Number of bus values depends on selection in Web configuration
-    {"xdrRoll", "xdrPitch"},// Bus values we need in the page
+    {"xdrROLL", "xdrPTCH"},// Bus values we need in the page
+   // {"xdrRoll", "xdrPitch"},// Bus values we need in the page
     true                // Show display header on/off
 );
 
